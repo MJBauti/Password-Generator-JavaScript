bdabccfd59c00a6b89294d4af1df8dbce0f95a22
MJBauti <michael.bautista@yahoo.com>
2022-07-25 20:57:35 -0700

    implemented window prompts to set values

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,22 +13,64 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+const RandomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomSpecial
+};
 
-// Generate Random http://www.net-comber.charset.html
+function generatePassword() {
+  var generatedPassword = "";
+
+  // Have user define characters used
+  var passwordCharSet = "";
+
+  var length = window.prompt("Password Length (8-128):");
+
+  var hasLower = window.confirm("Include Lowercase?");
+  if (hasLower) {
+    passwordCharSet += RandomFunc.lower
+  }
+
+  var hasUpper = window.confirm("Include Uppercase?");
+  if (hasUpper) {
+    passwordCharSet += RandomFunc.upper
+  }
+
+  var hasNumber = window.confirm("Include Numbers?");
+  if (hasNumber) {
+    passwordCharSet += RandomFunc.number
+  }
+
+  var hasSpecial = window.confirm("Include Special Characters?");
+  if (hasSpecial) {
+    passwordCharSet += RandomFunc.special
+  }
+
+  var charSetArray = [{lower}, {upper}, {number}, {special}].filter(item => Object.values(item)[0]);
+
+  if(passwordCharSet === 0) {
+    return;
+  }
+}
+
+// Generate Random Characters
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() *26) + 97);
-}
+};
 
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() *26) + 65);
-}
+};
 
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() *10) + 48);
-}
+};
 
-function getRandomCharacter() {
-  var characters = `!@#$%^&*(){}[]=<>?,.'`
-  return characters[Math.floor(Math.random() * characters.length)];
-}
+function getRandomSpecial() {
+  var special = `!@#$%^&*(){}[]=<>?,.'`
+  return special[Math.floor(Math.random() * special.length)];
+};
+
