fad7d3aa15d5ab8129bd2711d0fdc10241010f2a
MJBauti <michael.bautista@yahoo.com>
2022-07-25 18:06:06 -0700

    created functions to get random inputs

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,3 +12,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Generate Random http://www.net-comber.charset.html
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+
+function getRandomCharacter() {
+  var characters = `!@#$%^&*(){}[]=<>?,.'`
+  return characters[Math.floor(Math.random() * characters.length)];
+}
